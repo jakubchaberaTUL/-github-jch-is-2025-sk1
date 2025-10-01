3d1dbc6b8d6b1bfb636fbbb2d462891fd93b39f0
Jakub Chabera <jakub.chabera@tul.cz>
2025-10-01 10:27:25 +0200

    #3  Úkol 003 - první program v C#

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,15 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Write("Program na vypsání číselné řady.\n");
+    Console.Write("Zadejte cislo: ");
+
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Neplatny vstup. Zadejte cislo: ");
+    }
+
+    Console.Write("Pro opakovani stisknete 'a', jinak jiny znak: ");
+    again = Console.ReadLine();
+}
