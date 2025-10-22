8ffb7c2996a5bb49ec52bfdf16c50c1b237c8bf5
Jakub Chabera <jakub.chabera@tul.cz>
2025-10-22 09:36:35 +0200

    #7 Vytvoření programu pro řazení čísel

--- IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -15,12 +15,34 @@ while (again == "a")
 
     //Vstup hodnoty do programu 
     Console.Write("Zadejte hodnotu (celé číslo): ");
-    int first;
+    int a;
 
-    while (!int.TryParse(Console.ReadLine(), out first))
+    while (!int.TryParse(Console.ReadLine(), out a))
     {
         Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
     }
+
+    int b;
+     Console.Write("Zadejte hodnotu (celé číslo): ");
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+
+    Console.WriteLine();
+    int pom;
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+        Console.WriteLine("Došlo k prohození hodnot.");
+    }
+
+    Console.WriteLine($"Seřazená čísla jsou: {a}, {b}");
+
+
 
 
     Console.WriteLine();
