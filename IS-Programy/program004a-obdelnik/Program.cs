f1a622937eb9f7f337b6b60875244bec34f0b89a
Jakub Chabera <jakub.chabera@tul.cz>
2025-10-22 10:26:26 +0200

    #7 Vytvoření projektu pro vykreslování obdelníku do konzole

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,55 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Vykreslení obdelníku *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Jakub Chabera ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu 
+    Console.Write("Zadejte šířku obrazce (celé číslo): ");
+    int width;
+
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+    Console.Write("Zadejte výšku obrazce (celé číslo): ");
+    int height;
+
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+
+
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= width; j++)
+        {
+            Console.Write("*");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(250)); // Pauza 250 ms mezi vykreslením jednotlivých hvězdiček
+        }
+        Console.WriteLine();
+    }
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
+
+
