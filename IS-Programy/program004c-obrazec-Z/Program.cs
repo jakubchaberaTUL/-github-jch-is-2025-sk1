dc9d5b89465f360239e78ffebbee8d1dac8b5898
Jakub Chabera <jakub.chabera@tul.cz>
2025-10-29 18:02:39 +0100

    #8 vytvoření 3 programů pro vykreslování obrazců

---
+++ IS-Programy/program004c-obrazec-Z/Program.cs
@@ -0,0 +1,42 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("***********************************");
+    Console.WriteLine("***** Vykreslení obrazce 'Z' ******");
+    Console.WriteLine("***********************************");
+    Console.WriteLine("********** Jakub Chabera **********");
+    Console.WriteLine("***********************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte velikost obrazce (celé číslo): ");
+    int size;
+    while (!int.TryParse(Console.ReadLine(), out size))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+    
+    Console.WriteLine(); 
+    
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (i == 0 || i == size - 1 || i + j == size - 1)
+            {
+                Console.Write("*");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+            
+            Thread.Sleep(TimeSpan.FromMilliseconds(5));
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+}
---
+++ IS-Programy/program004d-sachovnice/Program.cs
@@ -0,0 +1,43 @@
+﻿
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("***********************************");
+    Console.WriteLine("*** Vykreslení Šachovnice *******");
+    Console.WriteLine("***********************************");
+    Console.WriteLine("********** Jakub Chabera **********");
+    Console.WriteLine("***********************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte velikost obrazce (celé číslo): ");
+    int size;
+    while (!int.TryParse(Console.ReadLine(), out size))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+    
+    Console.WriteLine(); 
+    
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if ((i + j) % 2 != 0)
+            {
+                Console.Write("*");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+            
+            Thread.Sleep(TimeSpan.FromMilliseconds(5));
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+}
