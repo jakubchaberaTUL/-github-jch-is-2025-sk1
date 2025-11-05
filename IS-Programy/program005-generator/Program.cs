2e8b5b23cf64aacecd9d22b03303e4f23d0481e5
Jakub Chabera <jakub.chabera@tul.cz>
2025-11-05 09:50:19 +0100

    #9 generátor základ

--- IS-Programy/program005-generator/Program.cs
+++ IS-Programy/program005-generator/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("*Generátor pseudonáhodných čísel**");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Jakub Chabera ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu 
+    Console.Write("Zadejte počet generovaných čísel: ");
+    int n;
+
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste číslo. Zadejte hodnotu znovu: ");
+    }
+
+    Console.Write("Zadejte maximální hodnotu generovaných čísel: ");
+    int upperbound;
+
+    while (!int.TryParse(Console.ReadLine(), out upperbound))
+    {
+        Console.Write("Nezadali jste číslo. Zadejte hodnotu znovu: ");
+    }
+
+    Console.Write("Zadejte minimální hodnotu generovaných čísel: ");
+    int lowerbound;
+
+    while (!int.TryParse(Console.ReadLine(), out lowerbound))
+    {
+        Console.Write("Nezadali jste číslo. Zadejte hodnotu znovu: ");
+    }
+
+    Console.WriteLine("****************************");
+    Console.WriteLine("Zadané hodnoty:");
+    Console.WriteLine($"Počet generovaných čísel: {n}");
+    Console.WriteLine($"Maximální hodnota generovaných čísel: {upperbound}");
+    Console.WriteLine($"Minimální hodnota generovaných čísel: {lowerbound}");
+    Console.WriteLine("****************************");
+
+    int[] randomNumbers = new int[n];
+
+    Random rand = new Random();
+
+    Console.WriteLine("****************************");
+    Console.WriteLine("Pseudonáhodná čísla: ");
+
+
+    for (int i = 0; i < n; i++)
+    {
+        randomNumbers[i] = rand.Next(lowerbound, upperbound + 1);
+        Console.WriteLine("{0}", randomNumbers[i]);
+    }
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
+
+
