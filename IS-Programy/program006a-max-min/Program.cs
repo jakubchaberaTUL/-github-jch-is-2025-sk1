7ff5430b078cc1f3ee6820182d8a79cf99cd84ff
Jakub Chabera <jakub.chabera@tul.cz>
2025-11-12 09:31:32 +0100

    #10 Generátor čísel a hledání min max hodnot

--- IS-Programy/program006a-max-min/Program.cs
+++ IS-Programy/program006a-max-min/Program.cs
@@ -3,17 +3,13 @@ while (again == "a")
 {
     Console.Clear();
     Console.WriteLine("****************************");
-    Console.WriteLine("*Generátor pseudonáhodných čísel**");
+    Console.WriteLine("Generátor pseudonáhodných čísel");
     Console.WriteLine("****************************");
-    Console.WriteLine("******* Jakub Chabera ********");
+    Console.WriteLine("******* Jakub Chabera ******");
     Console.WriteLine("****************************");
     Console.WriteLine();
 
-    // Vstup hodnoty do programu - špatně řešený
-    //Console.Write("Zadejte první číslo řady: ");
-    //int first = int.Parse(Console.ReadLine());
 
-    //Vstup hodnoty do programu 
     Console.Write("Zadejte počet generovaných čísel: ");
     int n;
 
@@ -38,27 +34,17 @@ while (again == "a")
         Console.Write("Nezadali jste číslo. Zadejte hodnotu znovu: ");
     }
 
-    Console.WriteLine("****************************");
+    Console.WriteLine();
     Console.WriteLine("Zadané hodnoty:");
     Console.WriteLine($"Počet generovaných čísel: {n}");
     Console.WriteLine($"Maximální hodnota generovaných čísel: {upperbound}");
     Console.WriteLine($"Minimální hodnota generovaných čísel: {lowerbound}");
-    Console.WriteLine("****************************");
 
     int[] randomNumbers = new int[n];
 
-    // Počítadla kladných, záporných čísel a nul
-    int negativeNumbers = 0;
-    int positiveNumbers = 0;
-    int zeros = 0;
-
-    // sudá a lichá čísla
-    int evenNumbers = 0;
-    int oddNumbers = 0;
-
     Random rand = new Random();
 
-    Console.WriteLine("****************************");
+    Console.WriteLine();
     Console.WriteLine("Pseudonáhodná čísla: ");
 
 
@@ -66,45 +52,41 @@ while (again == "a")
     {
         randomNumbers[i] = rand.Next(lowerbound, upperbound + 1);
         Console.WriteLine("{0}", randomNumbers[i]);
-
-        if (randomNumbers[i] < 0)
-        {
-            negativeNumbers++;
-        }
-        else if (randomNumbers[i] > 0)
-        {
-            positiveNumbers++;
-        }
-        else
-        {
-            zeros++;
-        }
-        if (randomNumbers[i] % 2 == 0)
-        {
-            evenNumbers++;
-        }
-        else
-        {
-            oddNumbers++;
-        }
-
     }
 
-    Console.WriteLine("****************************");
-    Console.WriteLine($"Počet kladných čísel: {positiveNumbers}");
-    Console.WriteLine($"Počet záporných čísel: {negativeNumbers}");
-    Console.WriteLine($"Počet nul: {zeros}");
-    Console.WriteLine($"Počet sudých čísel: {evenNumbers}");
-    Console.WriteLine($"Počet lichých čísel: {oddNumbers}");
-    Console.WriteLine("****************************");
 
 
+    //Hledání maxima a minima
+    int max = randomNumbers[0];
+    int min = randomNumbers[0];
+    int posMax = 0;
+    int posMin = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (randomNumbers[i] > max)
+        {
+            max = randomNumbers[i];
+            posMax = i;
+        }
+        if (randomNumbers[i] < min)
+        {
+            min = randomNumbers[i];
+            posMin = i;
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("****************************");
+    Console.WriteLine($"Maximální hodnota: {max}");
+    Console.WriteLine($"Pozice maxima v poli: {posMax}");
+    Console.WriteLine($"Minimální hodnota: {min}");
+    Console.WriteLine($"Pozice minima v poli: {posMin}");
+    Console.WriteLine("****************************");
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
     again = Console.ReadLine();
 
-
 }
-
 
